1b860536250c9338197fafbd6334602345f2c046
Raphael <raphaelcrv@hotmail.com>
2022-09-27 22:28:11 -0300

    add comments

--- app.js
+++ app.js
@@ -23,6 +23,14 @@ function Header(props) {
 	);
 }
 
+//using class components to enable use the state 
+/*
+Note:
+Before React 16.8, function components did not have
+state or lifecycle hooks. With 16.8+, function components 
+can now use hooks to use state and you can implement 
+side-effects on first render and after every update.
+*/
 class Counter extends React.Component {
 
 	constructor() {
@@ -34,6 +42,7 @@ class Counter extends React.Component {
 
 	incrementScore = () =>{
 		console.log("increment");
+		//calling set state using callback to avoid the async issue
 		this.setState( prevState => {
 			return {score : prevState.score += 1}
 		})
@@ -78,7 +87,7 @@ const App = () => {
 		<div className="scoreboard">
 			<Header title="scoreBoard" totalPlayers={11} />
 			{/* Player list */}
-
+			{/* using map with JSX to render multiple players */}
 			{playersArray.map((el) => {
 				return <Players key={el.name} name={el.name} score={el.score}></Players>
 			})}
