f4ccefad4e5757f44b9c885d14268fe4f36bbc50
Raphael <raphaelcrv@hotmail.com>
2022-09-27 22:03:01 -0300

    refactor to class component and use set state with callback

--- app.js
+++ app.js
@@ -1,29 +1,73 @@
-function Header() {
+const playersArray = [
+	{
+		name: "Manolo",
+		score: 10
+	},
+	{
+		name: "amilka",
+		score : 20
+	}
+]
+
+function Header(props) {
+
+	const { title, totalPlayers } = props;
+
+	console.log(title);
+
 	return (
-			<header>
-				<h1>Score Board</h1>
-				<span className="stats">Players (1)</span>
-			</header>
+		<header>
+			<h1>{title}</h1>
+			<span className="stats">Players: {totalPlayers}</span>
+		</header>
 	);
 }
 
-const Counter = () =>{
-return (
-	<div className="counter">
-	<button className="counter-action decrement"> - </button>
-	<span className="counter-score">35</span>
-	<button className="counter-action increment"> +</button>
-</div>
-)
+class Counter extends React.Component {
+
+	constructor() {
+		super();
+		this.state = {
+			score : 0
+		};
+	}
+
+	incrementScore = () =>{
+		console.log("increment");
+		this.setState( prevState => {
+			return {score : prevState.score += 1}
+		})
+	}
+
+	decrementScore = ()=>{
+		console.log("decrement");
+		this.setState( nextState => {
+			return {score : nextState.score -= 1}
+		})
+	}
+
+	render(){
+		//const { score } = this.props
+		return (
+			<div className="counter">
+				<button onClick={this.decrementScore} className="counter-action decrement"> - </button>
+				<span className="counter-score">{this.state.score}</span>
+				<button onClick={this.incrementScore} className="counter-action increment"> +</button>
+			</div>
+		)
+	}
+	
 }
 
-const Players = () =>{
+const Players = (props) => {
+
+	const { name, score } = props;
 	return (
 		<div className="player">
-		<span className="player-name">
-			Guil
-		</span>
-		<Counter></Counter>
+			<span className="player-name">
+				{name}
+			</span>
+			<Counter score={score}></Counter>
 
 		</div>
 	)
@@ -32,8 +76,12 @@ const Players = () =>{
 const App = () => {
 	return (
 		<div className="scoreboard">
-			<Header />
-			<Players />
+			<Header title="scoreBoard" totalPlayers={11} />
+			{/* Player list */}
+
+			{playersArray.map((el) => {
+				return <Players key={el.name} name={el.name} score={el.score}></Players>
+			})}
 		</div>
 	)
 }
